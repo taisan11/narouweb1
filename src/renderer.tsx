7f44f45421f2069602d8bab908e8fb1e875fba4a
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-02-20 20:47:55 +0900

    viteなんてなかった

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,24 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
-import css from "./style.css?url"
+import {css,Style} from "hono/css"
+
+const maincss = css`body {
+    max-width: 800px;
+    margin: 0 auto;
+}
+@media (max-width: 800px) {
+    body {
+        margin: 0;
+    }
+}`
 
 export const renderer = jsxRenderer(({ children }) => {
   return (
     <html lang="ja">
       <head>
-        <link href={css} rel="stylesheet" />
+        <Style/>
+        {/* <link href={maincss} rel="stylesheet" /> */}
       </head>
-      <body>{children}</body>
+      <body class={maincss}>{children}</body>
     </html>
   )
 })
